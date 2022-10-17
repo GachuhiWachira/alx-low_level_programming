f2ea5662b02ad778a9083b10d39c5171a2dc5af1
Denis Wachira <denis025wachira@gmail.com>
2022-10-16 17:23:15 -0700

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in reverse in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'z'; alpha >= 'a'; alpha--)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+
+	return (0);
+}
