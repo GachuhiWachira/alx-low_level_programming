45f89ded6c4be4a05661ef64c71829118ea0f689
Denis Wachira <denis025wachira@gmail.com>
2022-10-16 17:00:51 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Print alphabet on a line omitting e and q 
+ * main - Print alphabet on a line omitting e and q
  * Return: 0
  */
 int main(void)
@@ -11,9 +11,10 @@ int main(void)
 	{
 		putchar(alpha);
 		alpha++;
-		if (alpha == 'e' || alpha == 'q')
-			alpha++;
+	if (alpha == 'e' || alpha == 'q')
+		alpha++;
 	}
 	putchar('\n');
+
 	return (0);
 }
