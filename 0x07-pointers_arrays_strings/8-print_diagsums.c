2092493591356900122665493d0cf80e4a35ce62
Denis Wachira <denis025wachira@gmail.com>
2022-11-02 15:03:07 -0700

    Tasks 7 & 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -17,7 +17,8 @@ void print_diagsums(int *a, int size)
 	sum1 = 0;
 	sum2 = 0;
 
-	i = 0;while (i < size)
+	i = 0;
+	while (i < size)
 	{
 		sum1 = sum1 + *(a + i * size + i);
 		sum2 = sum2 + *(a + i * size + size - i - 1);
